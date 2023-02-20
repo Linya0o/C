e7905f46aa198fe8c31ff5976f85f63b44c89cfd
linya0o <linya0o@163.com>
2023-02-20 20:03:15 +0800

    chapters 10

--- src/main.c
+++ src/main.c
@@ -2,42 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// 泛型宏
-#define logger(x) _Generic(x,\
-        int:loggeri,         \
-        long:loggerl,        \
-        float:loggerf,       \
-        double:loggerd)(x)
-
-void loggeri(int n);
-void loggerl(long n);
-void loggerf(float n);
-void loggerd(double n);
-void loggerld(long double n);
-
 int main(int argc, char *argv[]) {
-    int x = 3;
-    logger(x);
 
 	return EXIT_SUCCESS;
-}
-
-void loggeri(int n) {
-    printf("n: %d.\n", n);
-}
-
-void loggerl(long n) {
-    printf("n: %ld.\n", n);
-}
-
-void loggerf(float n) {
-    printf("n: %f.\n", n);
-}
-
-void loggerd(double n) {
-    printf("n: %f.\n", n);
-}
-
-void loggerld(long double n) {
-    printf("n: %Lf.\n", n);
 }
